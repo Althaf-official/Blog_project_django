4db93bd17210f9993d5aa4c983e689021e1aa54f
Althaf Sharafudheen <105126131+Althaf-official@users.noreply.github.com>
2023-08-21 17:43:04 +0400

    $python manage.py createsuperuser

--- blog/article/models.py
+++ blog/article/models.py
@@ -1,8 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
+# Define a Category model to categorize articles
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+# Define the Article model
 class Article(models.Model):
-    title = models.CharField(max_length=120)
+    title = models.CharField(max_length=200)
     content = models.TextField()
-    active  = models.BooleanField(default=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    categories = models.ManyToManyField(Category)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
